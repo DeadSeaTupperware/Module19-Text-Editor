841bda26be2edcd9d56b71f36e1aacd2f375817f
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-09-20 04:24:03 -0400

    funcrtional indexdb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,7 @@
+/* DEPENDENCIES */
 import { openDB } from "idb";
 
+/* FUNCTIONS */
 const initdb = async () =>
   openDB("jate", 1, {
     upgrade(db) {
@@ -12,24 +14,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+/* Accepts content and adds it to the database */
 export const putDb = async (content) => {
   const db = await openDB("jate", 1); // Connect to db
   const tx = db.transaction("jate", "readwrite"); // Create transaction
-  const store = tx.objectStore("todos"); // Open desired object store
-  const request = store.add({ todo: content }); // Add to database
+  const store = tx.objectStore("jate"); // Open desired object store
+  const request = store.put({ todo: content }); // Add to database
   const result = await request; // Confirm
   console.log("Result:", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+/* Get all the content from the database */
 export const getDb = async () => {
   const db = await openDB("jate", 1); // Connect to db
   const tx = db.transaction("jate", "readonly"); // Create transaction
-  const store = tx.objectStore("todos"); // Open desired object store
+  const store = tx.objectStore("jate"); // Open desired object store
   const request = store.getAll(); // Add to database
   const result = await request; // Confirm
   console.log("Result:", result);
 };
 
+/* INITIALIZER */
 initdb();
